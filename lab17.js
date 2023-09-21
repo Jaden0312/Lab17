86e3a7980a96af7bc81bb64f9187d5af60ae8185
Jaden0312 <eve0knight88@gmai.com>
2023-09-22 01:50:28 +0900

    update lab17

--- lab17.js
+++ lab17.js
@@ -1,18 +1,34 @@
 const slideImages = document.querySelectorAll('img');
-const nextBtn = document.getElementById('.next');
-const backBtn = document.getElementById('.back');
+const nextBtn = document.getElementById('next');
+const backBtn = document.getElementById('back');
 const dots = document.querySelectorAll('.dot');
 const currentImg = 1;
+var index = 0;
 
 nextBtn.addEventListener('click', ()=> {
-        slideImages.style.transform = 'translateX(-100%)'
-         // slideImages.style.left = '-100%';
-    console.log(nextBtn);
+    
+    index += 1;
+    console.log(index);
+    slideImages.forEach((slide) => {
+        // if (index >= 5) {
+            // slide.style.transform = 'translateX(0)';
+            slide.style.transform = `translateX(-${index % 5}00% )`;
+        // } else {
+            // slide.style.transform = `translateX(-${index}00% )`;
+        
+        console.log(slide.style.transform);
+        
+    })
+        
+        
 })
 
 
 backBtn.addEventListener('click', ()=> {
-    slideImages.style.transform = 'translate(100%)'
-    // slideImages.style.left = '100%';
-    console.log(backBtn);
+    index -= 1;
+    console.log('back', index);
+    slideImages.forEach((slide) => {
+        slide.style.transform = `translateX(-${index % 5}00% )`;
+        console.log('백',slide.style.transform);
+    });
 })
